96013030b12162ebde97f1c258d6a17e5f0b9487
braianricci <braianricci@gmail.com>
2025-11-25 01:49:26 -0300

    sessions, better oil, final fterminal, navigation tweaks

--- init.lua
+++ init.lua
@@ -13,101 +13,108 @@ vim.o.scrolloff = 10
 vim.o.sidescrolloff = 5
 vim.o.signcolumn = "yes"
 vim.o.winborder = "rounded"
+vim.o.laststatus = 3
 
 
 ----------------oO0 keymaps 0Oo----------------
 
 vim.g.mapleader = " "
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
+
+local function remap(context, shortcut, cmd)
+	local opts = { noremap = true, silent = true }
+	vim.keymap.set(context, shortcut, cmd, opts)
+end
+
+--save and restore session
+vim.keymap.set("n", "<leader>ss", function()
+    vim.cmd("mksession! " .. vim.fn.stdpath("config") .. "/sessions/session.vim")
+    print("Session saved")
+end)
+
+vim.keymap.set("n", "<leader>sl", function()
+    vim.cmd("source " .. vim.fn.stdpath("config") .. "/sessions/session.vim")
+    print("Session restored")
+end)
 
 -- write, save, write and save, source, clean search
-map("n", "<leader>w", ":write<CR>", opts)
-map("n", "<leader>q", ":quit<CR>", opts)
-map("n", "<leader>x", ":wq<CR>", opts)
-map("n", "<leader>o", ":update<CR> :source<CR>", opts)
-map("n", "<Esc>", ":<cmd>nohlsearch<CR>", opts)
+remap("n", "<leader>w", ":write<CR>")
+remap("n", "<leader>q", ":quit<CR>")
+remap("n", "<leader>x", ":wq<CR>")
+remap("n", "<leader>o", ":update<CR> :source<CR>")
+remap("n", "<Esc>", ":nohlsearch<CR>")
 
 -- copy, cut and paste to/from system clipboard
-map({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
-map({ "n", "v", "x" }, "<leader>d", '"+d<CR>')
-map({ "n", "v", "x" }, "<leader>p", '"+p<CR>')
+remap({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
+remap({ "n", "v", "x" }, "<leader>d", '"+d<CR>')
+remap({ "n", "v", "x" }, "<leader>p", '"+p<CR>')
 
 -- splits creation, resizing, navigation
-map("n", "<leader>s", ":split<CR>", opts)
-map("n", "<leader>v", ":vsplit<CR>", opts)
-map("n", "<C-Left>", ":vertical resize +3<CR>", opts)
-map("n", "<C-Right>", ":vertical resize -3<CR>", opts)
-map("n", "<C-Up>", ":horizontal resize +3<CR>", opts)
-map("n", "<C-Down>", ":horizontal resize -3<CR>", opts)
-map("n", "<BS>", "<C-w>h", opts) --C-h is backspace, so I have to rebind <BS> too
-map("n", "<C-h>", "<C-w>h", opts)
-map("n", "<C-j>", "<C-w>j", opts)
-map("n", "<C-k>", "<C-w>k", opts)
-map("n", "<C-l>", "<C-w>l", opts)
+remap("n", "<leader>s", ":split<CR>")
+remap("n", "<leader>v", ":vsplit<CR>")
+remap("n", "<C-Left>", ":vertical resize +3<CR>")
+remap("n", "<C-Right>", ":vertical resize -3<CR>")
+remap("n", "<C-Up>", ":horizontal resize +3<CR>")
+remap("n", "<C-Down>", ":horizontal resize -3<CR>")
+remap("n", "<BS>", "<C-w>h")           --C-h is backspace, so I have to rebind <BS> too
+remap({ "n", "v" }, "<C-h>", "<C-w>h") --move to left split
+remap({ "n", "v" }, "<C-j>", "<C-w>j") --move to down split
+remap({ "n", "v" }, "<C-k>", "<C-w>k") --move to up split
+remap({ "n", "v" }, "<C-l>", "<C-w>l") --move to right split
+remap("n", "<C-u>", "<C-u>zz")         --scroll up and center
+remap("n", "<C-d>", "<C-d>zz")         --scroll down and center
 
--- scroll
-map("n", "<C-u>", "<C-u>zz", opts)
-map("n", "<C-d>", "<C-d>zz", opts)
+-- copilot chat
+remap("n", "<leader>c", ":CopilotChat<CR>")
 
 -- insert esc, pairs
-map("i", "kj", "<Esc>", opts)
-map("i", "jk", "<Esc>", opts)
-map("i", "'", "''<left>", opts)
-map("i", '"', '""<left>', opts)
-map("i", "(", "()<left>", opts)
-map("i", "{", "{}<left>", opts)
-map("i", "[", "[]<left>", opts)
-map("i", "<", "<><left>", opts)
+remap("i", "kj", "<Esc>")
+remap("i", "jk", "<Esc>")
+remap("i", "'", "''<left>")
+remap("i", '"', '""<left>')
+remap("i", "(", "()<left>")
+remap("i", "{", "{}<left>")
+remap("i", "[", "[]<left>")
+remap("i", "<", "<><left>")
 
 -- visual map, move
-map("v", "<leader>r", "\"hy:%s/<C-r>h//g<left><left>", opts)
-map("v", "J", ":m '>+1<CR>gv=gv", opts)
-map("v", "K", ":m '>-2<CR>gv=gv", opts)
+remap("v", "<leader>r", "\"hy:%s/<C-r>h//g<left><left>")
+remap("v", "J", ":m '>+1<CR>gv=gv")
+remap("v", "K", ":m '>-2<CR>gv=gv")
 
 -- format
-map("n", "<leader>r", function() vim.lsp.buf.format({ async = true }) end, opts)
+remap("n", "<leader>r", function() vim.lsp.buf.format({ async = true }) end)
 
 -- pick files and help
-map("n", "<leader>ff", ":Pick files<CR>")
-map("n", "<leader>fh", ":Pick help<CR>")
+remap("n", "<leader>ff", ":Pick files<CR>")
+remap("n", "<leader>fh", ":Pick help<CR>")
 
 -- oil
-map("n", "<leader>e", function()
-	-- if oil, close it
-	if vim.bo.filetype == "oil" then
-		vim.cmd("q")
-		return
-	end
-
-	-- otherwise, open
-	require("oil").open_float()
-end, opts)
+remap("n", "<leader>e", function()
+	require("oil").toggle_float()
+end)
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "oil",
 	callback = function()
-		map("n", "<Esc>", ":q<CR>", { buffer = true })
-	end
+		vim.keymap.set("n", "<Esc>", ":q<CR>", { buffer = true, silent = true })
+	end,
 })
 
--- terminal
-map("t", "<Esc>", [[<C-\><C-n>]], opts)
-map("n", "<leader>1", ":Fterminal 1<CR>", opts)
-map("n", "<leader>2", ":Fterminal 2<CR>", opts)
-map("n", "<leader>3", ":Fterminal 3<CR>", opts)
-map("n", "<leader>4", ":Fterminal 4<CR>", opts)
-map("n", "<leader>5", ":Fterminal 5<CR>", opts)
+-- fterminal
+remap("t", "<Esc>", [[<C-\><C-n>]])
+remap("n", "<leader>1", ":Fterminal 1<CR>")
+remap("n", "<leader>2", ":Fterminal 2<CR>")
+remap("n", "<leader>3", ":Fterminal 3<CR>")
+remap("n", "<leader>4", ":Fterminal 4<CR>")
+remap("n", "<leader>5", ":Fterminal 5<CR>")
 
 -- tabs
-map("n", "<leader><Tab>", function()
-	vim.cmd("tabedit .")
-end, opts)
-map("n", "<Tab>", "gt", opts)
-map("n", "<S-Tab>", "gT", opts)
+remap("n", "<leader><Tab>", "<cmd>tabedit .<CR>")
+remap("n", "<Tab>", "gt")
+remap("n", "<S-Tab>", "gT")
 
 -- harpoon-like
-map("n", "<leader>a", function()
+remap("n", "<leader>a", function()
 	vim.cmd("argadd %")
 	vim.cmd("argdedup")
 end)
@@ -117,8 +124,8 @@ end)
 
 vim.pack.add({
 	-- themes
-	"https://github.com/ellisonleao/gruvbox.nvim",
 	"https://github.com/vague2k/vague.nvim",
+	"https://github.com/ellisonleao/gruvbox.nvim",
 	-- funtionalities
 	"https://github.com/karb94/neoscroll.nvim",
 	"https://github.com/stevearc/oil.nvim",
@@ -132,17 +139,17 @@ vim.pack.add({
 })
 
 require "neoscroll".setup({
-	duration_multiplier = 0.2,   -- Global duration multiplier
-  	easing = 'sine',           -- Default easing function	
+	duration_multiplier = 0.2, -- Global duration multiplier
+	easing = 'sine',        -- Default easing function	
 })
 require "mini.pick".setup()
 require "oil".setup()
 require "mason".setup()
 require "plenary"
-require "copilotchat".setup({
-	model = "gpt-4o",
-})
 require "fterminal" --mine
+require "copilotchat".setup({
+	model = "gpt-4.1",
+})
 
 vim.lsp.enable({ "lua_ls" })
 
@@ -151,7 +158,7 @@ vim.lsp.enable({ "lua_ls" })
 
 --vim.cmd("colorscheme vague")
 vim.cmd("colorscheme gruvbox")
-vim.cmd(":highlight StatusLine guibg=#a36f51 ctermbg=none")
+vim.cmd(":highlight StatusLine guibg=none ctermbg=none")
 vim.cmd(":highlight StatusLine guibg=none ctermbg=none")
 vim.cmd(":highlight Normal guibg=none ctermbg=none")
 vim.cmd(":highlight NormalNC guibg=none ctermbg=none")
@@ -162,6 +169,9 @@ vim.cmd(":highlight SignColumn guibg=none ctermbg=none")
 vim.cmd(":highlight winborder guibg=none ctermbg=none")
 vim.cmd(":highlight NormalFloat guibg=none ctermbg=none")
 vim.cmd(":highlight FloatBorder guibg=none ctermbg=none")
+vim.cmd(":highlight TabLineSel gui=underline ctermbg=none")
+vim.cmd(":highlight TabLine guibg=none ctermbg=none")
+vim.cmd(":highlight TabLineFill guibg=none ctermbg=none")
 
 vim.o.fillchars = "stl:─"
 vim.o.statusline = "%= %m[%f] %l/%L"
---
+++ sessions/session.vim
@@ -0,0 +1,77 @@
+let SessionLoad = 1
+let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-1 siso=-1
+let v:this_session=expand("<sfile>:p")
+silent only
+silent tabonly
+cd ~/appdata/local/nvim
+if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
+  let s:wipebuf = bufnr('%')
+endif
+let s:shortmess_save = &shortmess
+set shortmess+=aoO
+badd +29 init.lua
+badd +4 nvim-pack-lock.json
+badd +1 ~/Desktop/dev/balancer/server/index.js
+argglobal
+%argdel
+$argadd init.lua
+set stal=2
+tabnew +setlocal\ bufhidden=wipe
+tabrewind
+edit init.lua
+argglobal
+balt nvim-pack-lock.json
+setlocal foldmethod=manual
+setlocal foldexpr=v:lua.vim.treesitter.foldexpr()
+setlocal foldmarker={{{,}}}
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldenable
+silent! normal! zE
+let &fdl = &fdl
+let s:l = 30 - ((11 * winheight(0) + 13) / 26)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 30
+normal! 031|
+tabnext
+edit nvim-pack-lock.json
+argglobal
+balt init.lua
+setlocal foldmethod=manual
+setlocal foldexpr=0
+setlocal foldmarker={{{,}}}
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldenable
+silent! normal! zE
+let &fdl = &fdl
+let s:l = 4 - ((3 * winheight(0) + 13) / 26)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 4
+normal! 0
+tabnext 1
+set stal=1
+if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0 && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
+  silent exe 'bwipe ' . s:wipebuf
+endif
+unlet! s:wipebuf
+set winheight=1 winwidth=20
+let &shortmess = s:shortmess_save
+let s:sx = expand("<sfile>:p:r")."x.vim"
+if filereadable(s:sx)
+  exe "source " . fnameescape(s:sx)
+endif
+let &g:so = s:so_save | let &g:siso = s:siso_save
+set hlsearch
+nohlsearch
+doautoall SessionLoadPost
+unlet SessionLoad
+" vim: set ft=vim :
